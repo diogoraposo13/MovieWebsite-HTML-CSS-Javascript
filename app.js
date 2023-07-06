4c4ee3844bb0c8d9a702797d9d85cd899655f7d5
diogoraposo13 <diogo.raposo@live.com.pt>
2023-07-06 17:04:56 +0100

    Responsive website - Done

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ const movieLists = document.querySelectorAll(".movie-list");
 
 console.log(movieLists)
 
-arrows.forEach((arrow,i) => {
+arrows.forEach((arrow,i) => {    
 
     const itemNumber= movieLists[i].querySelectorAll("img").length;
     let clickCounter=0;
     arrow.addEventListener("click", () =>{
+        const ratio = Math.floor(window.innerWidth / 270);
         clickCounter++;
-        if(itemNumber-(5+clickCounter)>0){
+        if(itemNumber-(5+clickCounter) + (7 - ratio)>0){
             movieLists[i].style.transform=`translateX(${
                 movieLists[i].computedStyleMap().get("transform")[0].x.value
                 -300}px)`;
